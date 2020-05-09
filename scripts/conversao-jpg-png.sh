ff253ee95636cfe4daea29ef019741f938fa67bd
Junior <junior.jaava@gmail.com>
2020-05-09 18:48:10 +0200

    starting to implement convert script file

---
+++ scripts/conversao-jpg-png.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+convert /Users/josejunior/dev/git/shell-automated-script-project-1-local/shell-automated-script-project-1/imagens-livros/amazon_aws.jpg /Users/josejunior/dev/git/shell-automated-script-project-1-local/shell-automated-script-project-1/imagens-livros/amazon_aws.png
