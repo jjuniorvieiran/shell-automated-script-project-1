5b234d2e6781c8961371dcb7cf9525b49ac14381
Junior <junior.jaava@gmail.com>
2020-05-10 15:29:56 +0200

    passing array as a parameter; using for iterator

--- scripts/conversao-jpg-png.sh
+++ scripts/conversao-jpg-png.sh
@@ -2,5 +2,7 @@
 
 CAMINHO_IMAGENS=/Users/josejunior/dev/git/shell-automated-script-project-1-local/shell-automated-script-project-1/imagens-livros
 
-convert $CAMINHO_IMAGENS/$1.jpg $CAMINHO_IMAGENS/$1.png
-convert $CAMINHO_IMAGENS/$2.jpg $CAMINHO_IMAGENS/$2.png
+for imagem in $@
+do
+    convert $CAMINHO_IMAGENS/$imagem.jpg $CAMINHO_IMAGENS/$imagem.png
+done
