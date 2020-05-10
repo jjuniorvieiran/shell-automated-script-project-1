8eb1bc76ba53bdcb62c2f3cd9c6ea02765d92b0a
Junior <junior.jaava@gmail.com>
2020-05-10 17:25:11 +0200

    if condition; result of ls statement; in order to removing extension and saving in a specific directory

--- scripts/conversao-jpg-png.sh
+++ scripts/conversao-jpg-png.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 cd /Users/josejunior/dev/git/shell-automated-script-project-1-local/shell-automated-script-project-1/imagens-livros
+if [ ! -d png ] # if not existes png directory 
+then
+    mkdir png
+fi
 
 for imagem in *.jpg
 do
-    convert $imagem $imagem.png
+    imagem_sem_extensao=$(ls $imagem | awk -F. '{ print $1 }') # using $( .. ) the result of ls should be stored in imagem_sem_ext...
+    convert $imagem_sem_extensao.jpg png/$imagem_sem_extensao.png
 done
