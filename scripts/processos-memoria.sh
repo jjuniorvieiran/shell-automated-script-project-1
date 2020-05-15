4ab5e058c463a720890099d69728d6b5121759ff
Junior <junior.jaava@gmail.com>
2020-05-15 17:20:06 +0200

    send report to a file

--- scripts/processos-memoria.sh
+++ scripts/processos-memoria.sh
@@ -3,5 +3,7 @@
 processos=$(ps -e -o pid -r | head -n 11 | grep '[0-9]')
 for pid in $processos
 do
-    echo $(ps -p $pid -o comm=)
+    nome_processo=$(ps -p $pid -o comm=)
+    echo -n $(date +%F,%H:%M:%S,) >> $nome_processo.log # >> to not replace the file. to add new entries
+    # -n is to not break the line
 done
